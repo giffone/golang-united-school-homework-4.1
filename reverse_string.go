721b1c15feda2b42656cd1c184e92f997d6753bc
gfaiz <giffo@mail.ru>
2022-05-19 17:37:24 +0600

    add

--- reverse_string.go
+++ reverse_string.go
@@ -1,12 +1,16 @@
 package reverse_string
 
-// func ReverseString(input string) (output string) {
-// 	b := bytes.Buffer{}
-// 	for i := len(input) - 1; i >= 0; i-- {
-// 		b.WriteByte(input[i])
-// 	}
-// 	return b.String()
-// }
+func ReverseString(input string) (output string) {
+	r := []rune(input)
+	lR := len(r)
+	s := make([]rune, lR)
+	i := 0
+	for index := lR - 1; index >= 0; index++ {
+		s[i] = r[index]
+		i++
+	}
+	return string(s)
+}
 
 // func ReverseString(input string) (output string) {
 // 	b := []byte{}
@@ -16,12 +20,12 @@ package reverse_string
 // 	return string(b)
 // }
 
-func ReverseString(input string) (output string) {
-	b := make([]byte, len(input))
-	i := 0
-	for index := len(input) - 1; index >= 0; index-- {
-		b[i] = input[index]
-		i++
-	}
-	return string(b)
-}
+// func ReverseString(input string) (output string) {
+// 	b := make([]byte, len(input))
+// 	i := 0
+// 	for index := len(input) - 1; index >= 0; index-- {
+// 		b[i] = input[index]
+// 		i++
+// 	}
+// 	return string(b)
+// }
