2feac195418204efec17a5d4fd0079d3929d722e
gfaiz <giffo@mail.ru>
2022-05-19 17:19:46 +0600

    add

--- reverse_string.go
+++ reverse_string.go
@@ -8,10 +8,20 @@ package reverse_string
 // 	return b.String()
 // }
 
+// func ReverseString(input string) (output string) {
+// 	b := []byte{}
+// 	for i := len(input) - 1; i >= 0; i-- {
+// 		b = append(b, input[i])
+// 	}
+// 	return string(b)
+// }
+
 func ReverseString(input string) (output string) {
-	b := []byte{}
-	for i := len(input) - 1; i >= 0; i-- {
-		b = append(b, input[i])
+	b := make([]byte, len(input))
+	i := 0
+	for index := len(input) - 1; index >= 0; index-- {
+		b[i] = input[index]
+		i++
 	}
 	return string(b)
 }
