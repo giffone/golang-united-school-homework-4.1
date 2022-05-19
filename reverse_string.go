f775d36246c917a1eea692d24be605d31d3118e0
gfaiz <giffo@mail.ru>
2022-05-19 16:48:54 +0600

    add

--- reverse_string.go
+++ reverse_string.go
@@ -1,19 +1,17 @@
 package reverse_string
 
-import "bytes"
+// func ReverseString(input string) (output string) {
+// 	b := bytes.Buffer{}
+// 	for i := len(input) - 1; i >= 0; i-- {
+// 		b.WriteByte(input[i])
+// 	}
+// 	return b.String()
+// }
 
 func ReverseString(input string) (output string) {
-	b := bytes.Buffer{}
+	b := []byte{}
 	for i := len(input) - 1; i >= 0; i-- {
-		b.WriteByte(input[i])
+		b = append(b, input[i])
 	}
-	return b.String()
+	return string(b)
 }
-
-// func ReverseString(input string) (output string) {
-// 	b := []byte{}
-// 	for i := len(input) - 1; i >= 0; i-- {
-// 		b = append(b, input[i])
-// 	}
-// 	return string(b)
-// }
